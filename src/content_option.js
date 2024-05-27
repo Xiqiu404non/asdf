c9d6618def4da25906a3777675adf107c8717d6e
Xiqiu <119796181+Xiqiu404non@users.noreply.github.com>
2024-05-28 00:03:26 +0800

    Update content_option.js

--- src/content_option.js
+++ src/content_option.js
@@ -113,9 +113,10 @@ const dataportfolio = [{
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_EMAIL: "0515claire.lai@gmail.com",
+    YOUR_FONE: "+886 968066886",
+    description: "歡迎聯絡IG討論花款𓍢ִ໋🌷͙֒ ᰔ",
+
     YOUR_SERVICE_ID: "service_id",
     YOUR_TEMPLATE_ID: "template_id",
     YOUR_USER_ID: "user_id",
