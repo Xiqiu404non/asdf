394ecd98960d8bc8bd599ff7f8f19114dd93c5bb
Xiqiu <119796181+Xiqiu404non@users.noreply.github.com>
2024-05-27 16:23:22 +0800

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,143 @@
+const logotext = "Bahati";
+const meta = {
+    title: "@bahati_flora",
+    description: "𝓣𝓱𝓮 𝓯𝓵𝓸𝔀𝓮𝓻 𝓼𝓱𝓸𝓹 𝓲𝓼 𝓬𝓵𝓸𝓼𝓮𝓭, 𝓫𝓾𝓽 𝓽𝓱𝓮 𝓯𝓵𝓸𝔀𝓮𝓻𝓼 𝓬𝓸𝓷𝓽𝓲𝓷𝓾𝓮 𝓽𝓸 𝓫𝓵𝓸𝓸𝓶.",
+};
+
+const introdata = {
+    title: "@bahati_flora",
+    animated: {
+        first: "花語花尋",
+        second: "台北花店．永生花．乾燥花",
+        third: "  "
+    },
+    description: "採預購制無現貨 / 聯絡以詢問價格、訂購 / 可帶圖討論花款",
+    your_img_url: "https://drive.google.com/file/d/1_n0mFhwAbs57rOhtl4BcjPy66d6KgRB7/view?usp=sharing",
+};
+
+const dataabout = {
+    title: "店內花材",
+    aboutme: "1. 使用永生花/乾燥花/香皂花，保存在環境乾燥處可放置2~4年" +
+             "2. 永生玫瑰花為「厄瓜多玫瑰」，一朵直徑約6-7公分屬於非常頂級的大型玫瑰，色澤十分飽滿" +
+             "是由真花特殊處理製成，有天然紋路、裂痕屬正常現象每朵花都會經過認真的挑選，老闆品質認證"
+};
+
+
+const worktimeline = [{
+        jobtitle: "週六",
+        where: "東門捷運站",
+        date: "10:00-20:00",
+    },
+    {
+        jobtitle: "週日",
+        where: "東門捷運站",
+        date: "10:00-16:00",
+    },
+];
+
+const services = [{
+        title: "燈串",
+        description: "加購價$40｜無須電池，若需幫忙組裝會多收25元手續費，晚上送禮更浪漫。",
+    },
+    {
+        title: "透明提袋",
+        description: "加購價$50｜送禮方便攜帶，保存也簡單。",
+    },
+    {
+        title: "客製化",
+        description: "提供客製化「花色/緞帶/波浪紗/玻璃紙」等細項部分，都可以私訊討論。",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "name@domain.com",
+    YOUR_FONE: "(555)123-4567",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
