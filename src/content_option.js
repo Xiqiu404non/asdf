e7fef1ed2f4657d6f7adf448e74d9e555cd79b01
Xiqiu <119796181+Xiqiu404non@users.noreply.github.com>
2024-05-27 17:01:49 +0800

    Update content_option.js

--- src/content_option.js
+++ src/content_option.js
@@ -12,7 +12,7 @@ const introdata = {
         third: "  "
     },
     description: "採預購制無現貨 / 聯絡以詢問價格、訂購 / 可帶圖討論花款",
-    your_img_url: "https://drive.google.com/file/d/1_n0mFhwAbs57rOhtl4BcjPy66d6KgRB7/view?usp=sharing",
+    your_img_url: "https://ibb.co/nzbjD87",
 };
 
 const dataabout = {
