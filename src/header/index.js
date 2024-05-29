4df0c55c88b50911f88e1a688a7e2e9b0806aecd
Xiqiu <119796181+Xiqiu404non@users.noreply.github.com>
2024-05-29 15:05:13 +0800

    Update index.js

--- src/header/index.js
+++ src/header/index.js
@@ -41,7 +41,7 @@ const Headermain = () => {
                   <Link onClick={handleToggle} to="/about" className="my-3">About</Link>
                   </li>
                   <li className="menu_item">
-                  <Link  onClick={handleToggle} to="/portfolio" className="my-3">Product</Link>
+                  <Link  onClick={handleToggle} to="/product" className="my-3">Product</Link>
                   </li>
                   <li className="menu_item">
                   <Link onClick={handleToggle} to="/contact" className="my-3">Contact</Link>
